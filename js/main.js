4fd69a405af5d09c805500c4ed85ae274068fa2d
ChoiSungJu <106740067+91yab@users.noreply.github.com>
2022-06-02 19:48:35 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+  $('.top_banner i').on('click', function () {
+    $('.top_banner').slideUp();
+  });
+
+  $('.main_slider').on('init relint afterChange', function (e, s, c) {
+    console.log(e, s, c);
+    var TG = $('.slick-current');
+    TG.addClass('xxx').siblings().removeClass('xxx');
+    $('.num').text((c ? c + 1 : 1) + '/' + s.slideCount);
+  });
+
+  $('.main_slider').slick({
+    // arrows: false
+    autoplay: true,
+    autopalySpeed: 4000,
+    pauseOnHover: false,
+    dots: true,
+  });
+});
